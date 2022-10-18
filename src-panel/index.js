908f29857fcf70b510bb7b4ff94047db43492ec0
Liron Toledo <ldoggy20@gmail.com>
2022-10-18 18:34:46 +0200

    Basic version of close captions seems to be working

--- src-panel/index.js
+++ src-panel/index.js
@@ -4,17 +4,123 @@ import { createRoot } from 'react-dom/client'
 /** Main app */
 class App extends React.PureComponent {
 
+    state = {
+        transcripts: [],
+        transcript: '',
+        isMuted: false,
+    }
+
     componentDidMount(){
-        
+
+        // Initilaise reference to Web Speech API
+        const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+        const SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
+        const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+        // Create speech recognition and grammar object
+        const speechRecognitionList = new SpeechGrammarList();
+        this.recognition = new SpeechRecognition();
+
+        // Set recognition grammar (dictionary of words) to default grammar list
+        this.recognition.grammars = speechRecognitionList;
+        // Determines whether continuous results are captured (true), or just a single result each time recognition is started (false).
+        this.recognition.continuous = true;
+        // Sets the language of the recognition.
+        this.recognition.lang = 'en';
+        // Determines whether the speech recognition system should return interim results, or just final results.
+        this.recognition.interimResults = true;
+        // Sets the number of alternative potential matches that should be returned per result.
+        this.recognition.maxAlternatives = 1;
+
+        // Get all transcripts 
+         window.parent.postMessage({action: 'get-transcripts'}, '*')
+
+        // Start Recognition if unmuted
+        if(!this.state.isMuted) {
+            console.debug("Recognition Start")
+            this.recognition.start()
+        }
+
+        // Executes when receiving results from speech recognition
+        this.recognition.onresult = (event) => {
+            for (let i = event.resultIndex; i < event.results.length; i++) {
+                if (event.results[i][0].confidence <= 0) continue;
+                
+                if (event.results[i].isFinal && event.results[i][0].confidence >= 0.3) {
+                    let char = this.state.transcript ? '. ' : ''
+                    let transcript = event.results[i][0].transcript
+                    let finalTranscript = transcript.charAt(0).toUpperCase() + transcript.slice(1)
+                    this.setState({transcript: this.state.transcript + char + finalTranscript})
+                    window.parent.postMessage({action: 'update-transcripts', text: finalTranscript}, '*')
+                    // console.debug(this.state.transcript)
+                }
+            }
+            
+        }
+
+        // Executes when speech recognition ends
+        this.recognition.onspeechend = () => {
+            console.debug("Recognition Stop")
+            this.recognition?.stop()
+        }
+
+        // Executes when speech recognition cannot find any match
+        this.recognition.onnomatch = (event) => {
+            console.warn("No Recognition Match")
+        }
+
+        // Executes when speech recognition produces an error
+        this.recognition.onerror = (event) => {
+            console.error(`Error occurred in recognition: ${event.error}`)
+        }
+
+        window.addEventListener('message', e => {
+
+            if(e.data.action == 'send-transcripts'){
+                this.setState({transcripts: e.data.transcripts})
+            }
+
+            if(e.data.action == 'is-muted'){
+                if(this.recognition){
+
+                    if(!e.data.isMuted && this.state.isMuted){
+                        console.debug("Recognition Start")
+                        this.setState({isMuted: false})
+                        this.recognition.start()
+                    }
+                    else if(e.data.isMuted && !this.state.isMuted){
+                        console.debug("Recognition Stop")
+                        this.setState({isMuted: true})
+                        this.recognition.stop()
+
+                    }
+
+                }
+            }
+        })
+
+        // setInterval(this.getTranscripts, 250)
+        window.parent.postMessage({action: 'get-transcripts'}, '*')
+
+    }
+
+    componentWillUnmount(){
+       this.recognition?.stop()
+    }
+    
+    /** Fetches array of all transcripts from plugin */
+    getTranscripts= () => {
+        window.parent.postMessage({action: 'get-transcripts'}, '*')
     }
 
     /** Render */
     render = () => <>
-    
-        <center>
-            <img src={require('./waving-hand.svg')} style={{ height: 48, marginTop: 40, marginBottom: 20 }} />
-            <div style={{ color: 'white', fontFamily: 'sans-serif', fontSize: 15 }}>Hello from React!</div>
-        </center>
+
+        {this.state.transcripts.map(transcript => <> 
+            <div style={{color: 'white', fontSize: 16}}>{transcript.name}</div>
+            <div style={{color: 'white', fontSize: 12, margin: '10px 0px'}}>{transcript.text}</div>
+        </>)}
+       
     
     </>
 
--- src/index.js
+++ src/index.js
@@ -14,18 +14,13 @@ export default class ClosedCaptionPlugin extends BasePlugin {
     static name = "Closed Captions"
     static description = "Enables ability to provide closed captions for microphone audio"
 
+    transcripts = []
+
     /** Called on load */
-    onLoad() {
+    async onLoad() {
 
         // Create a button in the toolbar
-        // this.menus.register({
-        //     icon: this.paths.absolute('button-icon.png'),
-        //     text: 'CC',
-        //     action: () => this.onButtonPress()
-        // })
-
-         // Create a button in the toolbar
-         this.menus.register({
+        this.menus.register({
             icon: this.paths.absolute('subtitles.svg'),
             text: 'Captions',
             panel: {
@@ -33,14 +28,60 @@ export default class ClosedCaptionPlugin extends BasePlugin {
             }
         })
 
+        this.userID = await this.user.getID()
+        this.userName = await this.user.getDisplayName()
+        this.transcripts = await this.getField('transcripts') || []
+        
+        console.group("TRANSCRIPTS")
+        console.log(this.transcripts)
+        console.groupEnd()
+
+        setInterval(this.isUserMuted, 100)
     }
 
-    /** Called when the user presses the action button */
-    onButtonPress() {
+    onMessage = async e => {
 
-        // Show alert
-        this.menus.alert(`Hello from ${MyPlugin.name} version ${require('../package.json').version}!`, 'Hello world!', 'info')
+        // Send transcripts to panel
+        if(e.action == 'get-transcripts') {
+            this.menus.postMessage({action: 'send-transcripts', transcripts: this.transcripts}, '*')
+        }
+
+        // Update saved transcripts
+        if(e.action == 'update-transcripts') {
+            
+            let index = this.transcripts.findIndex(t => t.id == this.userID);
+            
+            if(index == -1) {
+                let transcript = {id: this.userID, name: this.userName, text: e.text}
+                this.transcripts.push(transcript)
+            }
+            else {
+                this.transcripts[index].text += e.text
+            }
+
+            await this.setField('transcripts', this.transcripts)
+            
+            this.menus.postMessage({action: 'send-transcripts', transcripts: this.transcripts}, '*')
+            this.messages.send({action: 'refresh-transcripts', userID: this.userID, transcripts: this.transcripts})
+        }
+
+        // Called when transcript have been changed
+        if(e.action == 'refresh-transcripts'){
+            
+            // Don't need to refresh if we sent the message
+            if(this.userID == e.userID){
+                return
+            }
+
+            this.transcripts = e.transcripts
+            this.menus.postMessage({action: 'send-transcripts', transcripts: e.transcripts}, '*')
+        }
 
     }
 
+    /** Returns true if user is muted, false if otherwise, Sends result to panel */
+    isUserMuted = async () =>{
+        let isMuted = await this.user.isMuted()
+        this.menus.postMessage({action: 'is-muted', isMuted: isMuted}, '*')
+    }
 }
